68165d1dc2bf5b5903abe18c3fab35ef1a35b44d
anaghac123 <31351179+anaghac123@users.noreply.github.com>
2017-10-06 23:01:48 +0530

    Add files via upload

---
+++ lab4.question2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+void print(int i,int n)
+{
+	if(i==n)
+	{
+		cout<<i;
+	}
+	else
+	{
+		cout<<i<<" ";
+		print(i+1,n);
+	}
+    
+}
+int main()
+{
+int x,y;
+cout<<"number "<<endl;
+cin>> x;
+y=1;
+print(y,x);
+return 0;
+}
